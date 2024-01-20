46cf8dbb5aded370266a09b7aeb4615b0b5d3072
Erhan Gündüz <erhangndz@live.com>
2024-01-20 19:14:04 +0300

    updated

---
+++ SignalR.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalR.WebUI.Dtos.TestimonialDtos;
+
+namespace SignalR.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly HttpClient _client;
+
+
+        public TestimonialController(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri("https://localhost:7135/api/");
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultTestimonialDto>>("testimonials");
+            return View(values);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _client.DeleteAsync($"testimonials/{id}");
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTestimonialDto createTestimonialDto)
+        {
+            await _client.PostAsJsonAsync("testimonials", createTestimonialDto);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var value = await _client.GetFromJsonAsync<UpdateTestimonialDto>($"testimonials/{id}");
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateTestimonialDto updateTestimonialDto)
+        {
+            await _client.PutAsJsonAsync("testimonials", updateTestimonialDto);
+
+            return RedirectToAction("Index");
+        }
+    }
+}

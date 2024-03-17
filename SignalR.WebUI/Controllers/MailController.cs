971a06fa5c061a71b129f16b80ece842ba306089
Erhan Gündüz <erhangndz@live.com>
2024-03-17 15:22:36 +0300

    updated

---
+++ SignalR.WebUI/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalR.WebUI.Dtos.MailDtos;
+
+namespace SignalR.WebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateMailDto createMailDto)
+        {
+            var mimeMessage = new MimeMessage();
+
+            var mailBoxAddressFrom = new MailboxAddress("Gündüz Restoran Rezervasyon", "gunduzrestoran@gmail.com");
+            mimeMessage.From.Add(mailBoxAddressFrom);
+
+            var mailBoxAddressTo = new MailboxAddress("Sayın Kullanıcı", createMailDto.ReceiverMail);
+            mimeMessage.To.Add(mailBoxAddressTo);
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = createMailDto.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject= createMailDto.Subject;
+            var client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("gunduzrestoran@gmail.com", "irxmszakvfekhaxl");
+
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return NoContent();
+        }
+    }
+}
---
+++ SignalR.WebUI/Dtos/MailDtos/CreateMailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.WebUI.Dtos.MailDtos
+{
+    public class CreateMailDto
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
